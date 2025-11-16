e801bb75809f4dc45ae8d375b8d925662374ce06
Rangdy-Kor <hwangseonug12@gmail.com>
2025-11-16 18:38:55 +0900

    ver 0.0.17

--- kernel/src/interrupts.rs
+++ kernel/src/interrupts.rs
@@ -2,26 +2,16 @@ use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
 use spin::Mutex;
-use core::ptr;
-use x86_64::structures::tss::TaskStateSegment;
-use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor, SegmentSelector};
-use x86_64::VirtAddr;
-use x86_64::instructions::port::Port;
 
 static mut SCANCODE_BUFFER: [u8; 16] = [0; 16];
 static mut BUFFER_HEAD: usize = 0;
 static mut BUFFER_TAIL: usize = 0;
 
-pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
-
 pub fn read_scancode() -> Option<u8> {
     unsafe {
-        let head = ptr::read_volatile(&BUFFER_HEAD);
-        let tail = ptr::read_volatile(&BUFFER_TAIL);
-        
-        if head != tail {
-            let scancode = ptr::read_volatile(&SCANCODE_BUFFER[head]);
-            ptr::write_volatile(&mut BUFFER_HEAD, (head + 1) % 16);
+        if BUFFER_HEAD != BUFFER_TAIL {
+            let scancode = SCANCODE_BUFFER[BUFFER_HEAD];
+            BUFFER_HEAD = (BUFFER_HEAD + 1) % 16;
             Some(scancode)
         } else {
             None
@@ -63,11 +53,7 @@ lazy_static! {
         idt.alignment_check.set_handler_fn(alignment_check_handler);
         idt.machine_check.set_handler_fn(machine_check_handler);
         idt.simd_floating_point.set_handler_fn(simd_fpu_handler);
-        
-        unsafe {
-            idt.double_fault.set_handler_fn(double_fault_handler)
-                .set_stack_index(DOUBLE_FAULT_IST_INDEX);
-        }
+        idt.double_fault.set_handler_fn(double_fault_handler);
         
         idt[InterruptIndex::Keyboard as usize].set_handler_fn(keyboard_interrupt_handler);
         
@@ -75,96 +61,23 @@ lazy_static! {
     };
 }
 
-lazy_static! {
-    static ref TSS: TaskStateSegment = {
-        let mut tss = TaskStateSegment::new();
-        tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
-            const STACK_SIZE: usize = 4096 * 5;
-            static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
-            
-            let stack_start = VirtAddr::from_ptr(unsafe { &STACK });
-            stack_start + STACK_SIZE
-        };
-        tss
-    };
-}
-
-lazy_static! {
-    static ref GDT: (GlobalDescriptorTable, Selectors) = {
-        let mut gdt = GlobalDescriptorTable::new();
-        let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
-        let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
-        (gdt, Selectors { code_selector, tss_selector })
-    };
-}
-
-struct Selectors {
-    code_selector: SegmentSelector,
-    tss_selector: SegmentSelector,
-}
-
 pub fn init_idt() {
     IDT.load();
 }
 
 pub fn init_pics() {
-    use vga_driver::{print_colored, Color};
-    
     unsafe { 
         PICS.lock().initialize();
-        PICS.lock().write_masks(0xFD, 0xFF);
-        
-        let mut pic1_data = Port::<u8>::new(0x21);
-        let mask = pic1_data.read();
-        
-        print_colored("PIC1 Mask: ", Color::Yellow, Color::Black);
-        // 마스크 값 출력 (16진수)
-        let hex_chars = b"0123456789ABCDEF";
-        let mut buf = [0u8; 1];
-        buf[0] = hex_chars[(mask >> 4) as usize];
-        if let Ok(s) = core::str::from_utf8(&buf) {
-            print_colored(s, Color::Yellow, Color::Black);
-        }
-        buf[0] = hex_chars[(mask & 0x0F) as usize];
-        if let Ok(s) = core::str::from_utf8(&buf) {
-            print_colored(s, Color::Yellow, Color::Black);
-        }
-        print_colored("\n", Color::Yellow, Color::Black);
+        PICS.lock().write_masks(0b11111101, 0b11111111);
     };
-}
-
-pub fn init_gdt() {
-    use x86_64::instructions::tables::load_tss;
-    use x86_64::instructions::segmentation::{CS, Segment};
-    
-    GDT.0.load();
-    unsafe {
-        CS::set_reg(GDT.1.code_selector);
-        load_tss(GDT.1.tss_selector);
-    }
 }
 
 pub fn enable_interrupts() {
     x86_64::instructions::interrupts::enable();
 }
 
-static mut TEST_VAR: u8 = 0;
-
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    static mut COUNTER: u8 = 0;
-    
-    unsafe {
-        // 스캔코드 읽기
-        let mut port = Port::new(0x60);
-        let _scancode: u8 = port.read();
-        
-        // 카운터 증가 (메모리 쓰기 테스트)
-        COUNTER = COUNTER.wrapping_add(1);
-        
-        // EOI
-        let mut pic_cmd = Port::<u8>::new(0x20);
-        pic_cmd.write(0x20);
-    }
+    // 완전히 비움
 }
 
 extern "x86-interrupt" fn breakpoint_handler(_stack_frame: InterruptStackFrame) {}
@@ -207,25 +120,14 @@ extern "x86-interrupt" fn stack_segment_handler(_stack_frame: InterruptStackFram
     loop { x86_64::instructions::hlt(); }
 }
 
-extern "x86-interrupt" fn general_protection_handler(stack_frame: InterruptStackFrame, error_code: u64) {
-    use vga_driver::{print_colored, Color, println};
-    print_colored("\n\nGENERAL PROTECTION FAULT!\n", Color::White, Color::Red);
-    println!("Error Code: {}", error_code);
-    println!("{:#?}", stack_frame);
+extern "x86-interrupt" fn general_protection_handler(_stack_frame: InterruptStackFrame, _error_code: u64) {
     loop { x86_64::instructions::hlt(); }
 }
 
 extern "x86-interrupt" fn page_fault_handler(
-    stack_frame: InterruptStackFrame,
-    error_code: x86_64::structures::idt::PageFaultErrorCode,
+    _stack_frame: InterruptStackFrame,
+    _error_code: x86_64::structures::idt::PageFaultErrorCode,
 ) {
-    use vga_driver::{print_colored, Color, println};
-    use x86_64::registers::control::Cr2;
-    
-    print_colored("\n\nPAGE FAULT!\n", Color::White, Color::Red);
-    println!("Accessed Address: {:?}", Cr2::read());
-    println!("Error Code: {:?}", error_code);
-    println!("{:#?}", stack_frame);
     loop { x86_64::instructions::hlt(); }
 }
 
@@ -245,14 +147,11 @@ extern "x86-interrupt" fn simd_fpu_handler(_stack_frame: InterruptStackFrame) {
     loop { x86_64::instructions::hlt(); }
 }
 
-
 extern "x86-interrupt" fn double_fault_handler(
-    stack_frame: InterruptStackFrame,
-    error_code: u64,
+    _stack_frame: InterruptStackFrame,
+    _error_code: u64,
 ) -> ! {
-    use vga_driver::{print_colored, Color, println};
-    print_colored("\n\nDOUBLE FAULT!\n", Color::White, Color::Red);
-    println!("Error Code: {}", error_code);
-    println!("{:#?}", stack_frame);
-    loop { x86_64::instructions::hlt(); }
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
